97d4b64090414f4dec00a959d26cf803c9508c36
Vladislav Kohai <cherry.designwork@gmail.com>
2023-04-23 23:40:12 +0300

    Arrays finish

--- app.ts
+++ app.ts
@@ -1,2 +1,8 @@
-const skills: string[] = ['HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'C#'];
-console.log(skills);
+let skills: string[] = ['HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'C#'];
+
+skills = skills.filter(s => !s.startsWith('C'));
+console.log("My skill list:")
+for (const skill of skills) {
+    console.log(skill.toUpperCase());
+}
+
