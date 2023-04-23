026a9c33ba348af3f62f90218d139b12ba6885a1
Vladislav Kohai <cherry.designwork@gmail.com>
2023-04-23 23:48:43 +0300

    Tuple finish

---
+++ app.ts
@@ -0,0 +1,5 @@
+let skill: [number, string] = [3, "TypeScript"];
+const [progress, skillName] = skill;
+// const progress: number = skill[0];
+// const skillName: string = skill[1];
+console.log(`${skillName} is aquired about ${progress}%`);
