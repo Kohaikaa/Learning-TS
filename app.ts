675ed5c04fe4c438b98a4343a562e5479f34f82f
Vladislav Kohai <cherry.designwork@gmail.com>
2023-04-23 23:41:40 +0300

    Tuples

--- app.ts
+++
@@ -1,8 +0,0 @@
-let skills: string[] = ['HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'C#'];
-
-skills = skills.filter(s => !s.startsWith('C'));
-console.log("My skill list:")
-for (const skill of skills) {
-    console.log(skill.toUpperCase());
-}
-
