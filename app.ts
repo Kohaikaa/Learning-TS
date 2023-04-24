53396468ed133cc42194c27a9ddc899748939237
Vladislav Kohai <cherry.designwork@gmail.com>
2023-04-24 13:18:31 +0300

    Test 2 start

--- app.ts
+++
@@ -1,5 +0,0 @@
-let skill: [number, string] = [3, "TypeScript"];
-const [progress, skillName] = skill;
-// const progress: number = skill[0];
-// const skillName: string = skill[1];
-console.log(`${skillName} is aquired about ${progress}%`);
